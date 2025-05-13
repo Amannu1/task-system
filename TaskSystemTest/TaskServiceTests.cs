b53174c2977cdfa7ee2c97ae9b6ff1154a4e689e
Luiz <manuercolle@gmail.com>
2025-05-13 14:15:17 -0300

    create Create, Update and Delete TaskTests

--- TaskSystemTest/TaskServiceTests.cs
+++ TaskSystemTest/TaskServiceTests.cs
@@ -19,7 +19,7 @@ namespace TaskSystemTest
         [Fact]
         public async Task GetAllShouldReturnTaskList()
         {
-            var list = new List<TaskModel> { new TaskModel{Id = 1, Name = "Task 01", Description = "Description 1", Status = (TaskStatus)1, UserId = 1}};
+            var list = new List<TaskModel> { new TaskModel { Id = 1, Name = "Task 01", Description = "Description 1", Status = (TaskStatus)1, UserId = 1 } };
             _repository.Setup(repo => repo.GetAll()).ReturnsAsync(list);
 
             var result = await _service.GetAll();
@@ -29,7 +29,7 @@ namespace TaskSystemTest
         }
 
         [Fact]
-        public async Task GetByIdShouldReturnExistingUser()
+        public async Task GetByIdShouldReturnUserWhenIdExists()
         {
             TaskModel task = new TaskModel { Id = 1, Name = "Task 01", Description = "Description 1", Status = (TaskStatus)1, UserId = 1 };
             _repository.Setup(repo => repo.GetById(task.Id)).ReturnsAsync(task);
@@ -39,7 +39,56 @@ namespace TaskSystemTest
             Assert.NotNull(result);
             Assert.Equal(task.Name, result.Name);
             Assert.Equal(task.Id, result.Id);
-    
+
+        }
+
+        [Fact]
+        public async Task GetByIdShouldThrowExceptionWhenIdDoesNotExist()
+        {
+            TaskModel task = new TaskModel { Id = 1, Name = "Task 01", Description = "Description 1", Status = (TaskStatus)1, UserId = 1 };
+            _repository.Setup(repo => repo.GetById(task.Id)).ReturnsAsync(task);
+
+            var ex = await Assert.ThrowsAsync<Exception>(() => _service.GetById(2));
+        }
+
+        [Fact]
+        public async Task CreateShouldReturnUser()
+        {
+            TaskModel task = new TaskModel { Id = 1, Name = "Task 01", Description = "Description 1", Status = (TaskStatus)1, UserId = 1 };
+            _repository.Setup(repo => repo.Create(task)).ReturnsAsync(task);
+
+            var result = await _service.Create(task);
+
+            Assert.NotNull(result);
+            Assert.Equal(task.Id, result.Id);
+        }
+
+        [Fact]
+        public async Task UpdateShouldReturnUser()
+        {
+            TaskModel task = new TaskModel { Id = 1, Name = "Task 01", Description = "Description 1", Status = (TaskStatus)1, UserId = 1 };
+            _repository.Setup(repo => repo.Update(task, task.Id)).ReturnsAsync(task);
+
+            var result = await _service.Update(task, task.Id);
+
+            Assert.NotNull(result);
+            Assert.Equal(task.Id, result.Id);
+            Assert.Equal(task.Name, result.Name);
+            Assert.Equal(task.Description, result.Description);
+            Assert.Equal(task.Status, result.Status);
+            Assert.Equal(task.UserId, result.UserId);
+
+        }
+
+        [Fact]
+        public async Task DeleteShouldReturnTrue()
+        {
+            TaskModel task = new TaskModel { Id = 1, Name = "Task 01", Description = "Description 1", Status = (TaskStatus)1, UserId = 1 };
+            _repository.Setup(repo => repo.Delete(task.Id)).ReturnsAsync(true);
+
+            var result = await _service.Delete(task.Id);
+
+            Assert.True(result);
         }
     }
 }
